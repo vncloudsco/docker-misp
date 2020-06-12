9a9a8cd859c6f1ab564a17d83afd083eeaaadaa1
Vi Vu <mtdev@manhtuong.net>
2020-06-12 11:14:47 +0700

    1

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 docker rmi harvarditsecurity/misp
 docker build \
     --rm=true --force-rm=true \
-    --build-arg MYSQL_MISP_PASSWORD=JBao5Q2bps89LWFqWkKgDZwAFpNHvc \
+    --build-arg MYSQL_MISP_PASSWORD=JBao5Q2bps89LWFqWkKgDZwAFpNHvc_change_me \
     --build-arg POSTFIX_RELAY_HOST=localhost \
     --build-arg MISP_FQDN=localhost \
     --build-arg MISP_EMAIL=tuongvm@funtap.vn \
-    --build-arg MISP_GPG_PASSWORD=JBao5Q2bps89LWFqWkKgDZwAFpNHvc \
+    --build-arg MISP_GPG_PASSWORD=JBao5Q2bps89LWFqWkKgDZwAFpNHvc_change_me \
     -t harvarditsecurity/misp container
 
